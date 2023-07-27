81a4d0b903f3b3351667b58f9b130149b9e67254
Henry C <hchiong@ucla.edu>
2023-07-27 19:45:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS", "React"];
+    var member = {
+        name: "John",
+        age: 23,
+        skills: skills,
+    };
+    // Write code here
+    return member.skills;
+    }  
